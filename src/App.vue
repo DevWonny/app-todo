1833ef6fdd2ea1d79c6428ef22183daf036b9039
DevWonny <cjfdnjs1994@naver.com>
2022-11-26 23:13:22 +0900

    feat : input component 퍼블리싱 구현

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <Header></Header>
+  <Input></Input>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-// import Input from "./components/Input.vue";
+import Input from "./components/Input.vue";
 // import List from "./components/List.vue";
 // import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Header: Header,
+    Input: Input,
   },
 };
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="inputBox">
+    <input type="text" placeholder="Type what you have to do" />
+
+    <span class="addContainer">
+      <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.inputBox {
+  background: #fff;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+input {
+  outline: none;
+  width: 84%;
+  border: none;
+  font-size: 0.9rem;
+}
+span {
+  float: right;
+  display: inline-block;
+  width: 3rem;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  border-radius: 0 5px 5px 0;
+  text-align: center;
+}
+</style>
