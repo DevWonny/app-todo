fe6466c115152ed19abf8bb2f9cf5331a18a9eb4
DevWonny <cjfdnjs1994@naver.com>
2022-11-26 22:29:52 +0900

    feat : component 파일 생성 및 header 구현

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+// import Input from "./components/Input.vue";
+// import List from "./components/List.vue";
+// import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Header: Header,
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #f6f6f8;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "TODO it!" };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: #302349;
+  font-size: 2.5rem;
+  text-align: center;
+}
+</style>
